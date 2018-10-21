375a526c7a8c417dde52068ff77cda91e47cd77b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-10-21 18:44:31 +0300

    setup.py: Add.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='micropython-uffmpeg',
+      version='0.1.1',
+      description="MicroPython bindings for FFmpeg video handling library",
+      long_description="MicroPython bindings for FFmpeg video handling library.",
+      url='https://github.com/pfalcon/micropython-uffmpeg',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+      py_modules=['uffmpeg'])
